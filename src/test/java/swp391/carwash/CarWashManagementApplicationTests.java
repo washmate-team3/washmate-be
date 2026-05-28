725e4f0f5eda3eef8915e2dac422b181ef11b3f1
B-L-Son <buileson2001@gmail.com>
2026-05-28 14:02:23 +0700

    Init: Setup enviroment BE

---
+++ src/test/java/swp391/carwash/CarWashManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package swp391.carwash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
